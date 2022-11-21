6ada94e9d91edcd0e38df77f78a60f8721143d2c
smessie <smessie@smessie.com>
2022-11-21 14:17:17 +0100

    feat: First working version of eye-mock

---
+++ index.js
@@ -0,0 +1,49 @@
+export async function n3reasoner(data, query, onlyDerivations = true) {
+    // Document and query to body of request
+    const inputBody = [];
+    inputBody.push(
+        `${encodeURIComponent("task")}=${encodeURIComponent(
+            onlyDerivations ? "derivations" : "deductive_closure"
+        )}`
+    );
+    inputBody.push(
+        `${encodeURIComponent("system")}=${encodeURIComponent("eye")}`
+    );
+
+    inputBody.push(
+        `${encodeURIComponent("formula")}=${encodeURIComponent(
+            `${data}\n${query}`
+        )}`
+    );
+
+    let result = await fetch("http://ppr.cs.dal.ca:3002/n3", {
+        headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: inputBody.join("&"),
+        method: "POST",
+        credentials: "omit",
+    });
+    const body = await result.body;
+    // Read all the data from the ReadableStream
+    const reader = body.getReader();
+    const decoder = new TextDecoder();
+    let response = "";
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const {done, value} = await reader.read();
+        if (done) {
+            break;
+        }
+        response += decoder.decode(value);
+    }
+    // String to JSON
+    const json = JSON.parse(response);
+
+    // Return the result
+    return json.output;
+}
+
+export default {
+    n3reasoner,
+}
